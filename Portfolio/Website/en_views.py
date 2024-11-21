709b84a29bb0ce783e5a55e00e3eccac342e0168
ThatGabrieleC <167902801+ThatGabrieleC@users.noreply.github.com>
2024-11-21 17:22:53 +0100

    Add files via upload

---
+++ Portfolio/Website/en_views.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template, request, current_app
+from flask_mail import Message
+import time
+
+en_views = Blueprint('en_views', __name__)
+
+@en_views.route('/')
+def english_home():
+    return render_template('en/home.html')
+
+@en_views.route('/contact', methods=['POST'])
+def contact_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        msg = Message(
+            subject="Someone wants to HIRE MEEE",
+            recipients=["elgabbuz@gmail.com"],
+            body=f"{name} got interested by my portfolio, his infos: \nName: {name},\nEmail: {email},\nMessage: {message} ",
+            sender=current_app.config['MAIL_USERNAME']
+        )
+
+        try:
+            mail = current_app.extensions['mail']
+            mail.send(msg)
+            return render_template('success.html', message="Message sent successfully!", redirect='You will be reidrected automatically in:')
+        except Exception as e:
+            return render_template('success.html', message=f'Failed to send the message. Error: {str(e)}', redirect='You will be reidrected automatically in:')
+    
---
+++ Portfolio/Website/it_views.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, render_template, request, current_app
+from flask_mail import Message
+
+it_views = Blueprint('it_views', __name__)
+
+@it_views.route('/')
+def italian_home():
+    return render_template('it/home.html')
+
+@it_views.route('/contact', methods=['POST'])
+def contact_form():
+    if request.method == 'POST':
+        name = request.get['name']
+        email = request.get['email']
+        message = request.get['message']
+
+        msg = Message(
+            subject="Qualcuno vuole ASSUMERMIIIII",
+            recipients=["elgabbuz@gmail.com"],
+            body=f"{name} è stato catturato dal mio portafoglio, his infos:\nName: {name},\nEmail: {email},\n Message: {message}",
+            sender=current_app.config['MAIL_USERNAME']
+        )
+
+        try:
+            mail = current_app.extensions['mail']
+            mail.send(msg)
+            return render_template('success.html', message="Messaggio inviato con successo", render="Sarai reindirizzato automaticamente in:")
+        except Exception as e:
+            return render_template('success.html', message=f"C'è stato un errore nell'invio del messaggio. Errore: {str(e)}", render="Sarai reindirizzato automaticamente in:")
